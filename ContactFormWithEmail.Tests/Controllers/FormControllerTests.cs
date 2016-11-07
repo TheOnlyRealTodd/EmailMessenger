e43c92121e9f200877af320defce394924123d38
Todd <toddcullum@gmail.com>
2016-11-06 23:47:16 -0800

    Add Form Controller tests for Email and Index actions

--- ContactFormWithEmail.Tests/Controllers/FormControllerTests.cs
+++ ContactFormWithEmail.Tests/Controllers/FormControllerTests.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using ContactFormWithEmail.Controllers;
+using ContactFormWithEmail.Models;
+using ContactFormWithEmail.Services;
+using ContactFormWithEmail.ViewModels;
+using Moq;
 using NUnit.Framework;
 
 namespace ContactFormWithEmail.Tests.Controllers
@@ -11,7 +16,68 @@ namespace ContactFormWithEmail.Tests.Controllers
     [TestFixture]
     class FormControllerTests
     {
-        //[Test]
+
+        [Test]
+        public void Index_Returns_Index_ViewResult()
+        {
+            //Arrange
+            Mock<MessageRepository> mockRepository = new Mock<MessageRepository>();
+            Mock<TheEmailService> mockTheEmailService = new Mock<TheEmailService>();
+            FormController controller = new FormController(mockRepository.Object,mockTheEmailService.Object);
+
+            //Act
+            ViewResult result = controller.Index();
+
+            //Assert
+            Assert.AreEqual("index",result.ViewName);
+        }
+
+        [Test]
+        public void Index_Returns_MessageViewModel_Instance()
+        {
+            //Arrange
+            Mock<MessageRepository> mockRepository = new Mock<MessageRepository>();
+            Mock<TheEmailService> mockTheEmailService = new Mock<TheEmailService>();
+            FormController controller = new FormController(mockRepository.Object, mockTheEmailService.Object);
+
+            //Act
+            ViewResult result = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOf<MessageViewModel>(result.Model);
+        }
+
+        [Test]
+        public void Emails_Returns_ListOfMessages()
+        {
+            //Arrange
+            Mock<IRepository<Message>> mockRepository = new Mock<IRepository<Message>>();
+            Mock<ITheEmailService> mockTheEmailService = new Mock<ITheEmailService>();
+            //Create a mock Message to be returned in place of DB data.
+            List<Message> listOfMessages = new List<Message>();
+            listOfMessages.Add(new Message
+            {
+                Body = "This is a mocked message.",
+                Id = 1,
+                RecipientEmail = "Test@Test.com",
+                SenderEmail = "ToddCullum@gmail.com",
+                SenderName = "Todd Cullum",
+                Subject = "Test Subject",
+                TimeStamp = DateTime.Now
+            });
+            //Tells the mocked up repo's GetAll action what to return.
+            mockRepository.Setup(r => r.GetAll()).Returns(listOfMessages);
+
+
+            FormController controller = new FormController(mockRepository.Object, mockTheEmailService.Object);
+
+            //Act
+            ViewResult result = controller.Emails();
+
+            //Assert
+            Assert.IsInstanceOf<List<Message>>(result.Model);
+
+        }
 
     }
 }
